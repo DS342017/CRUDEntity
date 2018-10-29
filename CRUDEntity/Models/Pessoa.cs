a2a2eda24ec7e65e8c6d70b187ab62d0167fc817
Elizeu Elieber Fachini <elizeuelieber@live.com>
2018-10-29 12:41:33 -0300

    Versão básica do CRUD

---
+++ CRUDEntity/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDEntity.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int IdPessoa { get; set; }
+        public String Nome { get; set; }
+        public String Cpf { get; set; }
+        public String Telefone { get; set; }
+    }
+}
---
+++ CRUDEntity/Models/PessoaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDEntity.Models
+{
+    public class PessoaContext:DbContext
+    {
+        public DbSet<Models.Pessoa> Pessoa { get; set; }
+    }
+}
